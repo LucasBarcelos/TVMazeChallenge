019980795759f2735d11078a10689257e6101cf2
Lucas Barcelos <lucastbarcelos@gmail.com>
2023-06-16 14:56:27 -0300

    First commit - Creating the project

---
+++ TVMazeChallenge/Supporting Files/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TVMazeChallenge
+//
+//  Created by Lucas Barcelos on 15/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
